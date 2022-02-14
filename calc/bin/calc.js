1b266e4c822c1c61dcb515bdf0953fcb9af84e69
erezcohen <erez.cohen@gmail.com>
2022-02-14 12:04:04 +0200

    switch working with files to async

--- calc/bin/calc.js
+++ calc/bin/calc.js
@@ -1,11 +1,13 @@
 const argsParser = require('../lib/args');
 const { calc } = require('../lib');
 
-try {
-  const { func, n } = argsParser(process);
-  const res = calc({func, n});
-  console.log('Result: ' + res);
-} catch (e) {
-  console.error('Error: ' + e.message);
-  process.exit(1);
-}
+(async () => {
+  try {
+    const { func, n } = argsParser(process);
+    const res = await calc({func, n});
+    console.log('Result: ' + res);
+  } catch (e) {
+    console.error('Error: ' + e.message);
+    process.exit(1);
+  }
+})()
--- calc/lib/jobs/fibonacci-memoized.js
+++ calc/lib/jobs/fibonacci-memoized.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 const { fibRecursion } = require('./fibonacci');
 
 const CACHE_FILE_PATH = './lib/jobs/fibonacci-memoized-cache.json';
 
 
-const fibRecursionMemoized = (n) => {
-
-
+const fibRecursionMemoized = async (n) => {
   const cacheStr = fs.existsSync(CACHE_FILE_PATH)
-    ? fs.readFileSync(CACHE_FILE_PATH, 'utf-8')
+    ? await readFile(CACHE_FILE_PATH, 'utf-8')
     : '{}';
   const cache = JSON.parse(cacheStr);
 
@@ -20,14 +21,15 @@ const fibRecursionMemoized = (n) => {
   const newVal = calcNewValue(cache, n);
 
   cache[n] = newVal;
+
   console.debug(`Adding new entry to cache. n: ${n}, value: ${newVal}`)
-  fs.writeFileSync(CACHE_FILE_PATH, JSON.stringify(cache, null, 2));
+  await writeFile(CACHE_FILE_PATH, JSON.stringify(cache, null, 2));
   return newVal;
 };
 
 const calcNewValue = (cache, n) =>
   (cache[n - 1] !== undefined && cache[n - 2] !== undefined )
-    ? (console.log(`About to reuse ${n-1}, value: ${cache[n - 1]} and ${n-2}, value: ${cache[n - 2]}`) || cache[n - 1] + cache[n - 2])
+    ? cache[n - 1] + cache[n - 2]
     : fibRecursion(n);
 
 
